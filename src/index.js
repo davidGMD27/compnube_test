506c6c57829ef61bb75da4b39fb299ed9ca734ed
GMD <cruzasulcharal5@gmail.com>
2025-10-15 14:59:44 -0600

    prueba con react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify';
+
+// Your code that uses Amplify
+Amplify.configure({
+    Auth: {
+        // ID de tu Identity Pool de Cognito
+        identityPoolId: "us-east-1:1fc9775e-fc1b-43d6-adfc-5cebcae585b1",
+        region: "us-east-1",
+    },
+    Storage: {
+        AWSS3: {
+            // Nombre de tu bucket de S3
+            bucket: "clasesgrabadasunitec2025",
+            region: "us-east-1",
+        },
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
